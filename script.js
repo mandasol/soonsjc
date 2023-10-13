67b47f98ee24722d877a36e3364136cd6bdd68c9
mandasol <143660504+mandasol@users.noreply.github.com>
2023-10-12 22:05:00 -0400

    Upload final

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ navigationToggle.addEventListener('click', () => {
     navigationToggle.classList.toggle('active')
     showcase.classList.toggle('active')
 })
+
+type = "text/javascript"
+/*jshint esversion: 6 */
+document.querySelectorAll('nav a')
+    .forEach(e => e.addEventListener('click', _ => change(e.dataset.id)));
+
+function change(n) {
+    let panels = document.querySelectorAll('main div');
+    panels.forEach(p => p.classList.remove('active'));
+    panels[n - 1].classList.add('active');
+}
